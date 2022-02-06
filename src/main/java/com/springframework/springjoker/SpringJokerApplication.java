7433742496a4e94f7ccf62e3f1ec74c02f9c3e32
Issac <issac725725@gmail.com>
2022-02-06 18:06:54 +1030

    Initial commit

---
+++ src/main/java/com/springframework/springjoker/SpringJokerApplication.java
@@ -0,0 +1,13 @@
+package com.springframework.springjoker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJokerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringJokerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/springframework/springjoker/SpringJokerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springframework.springjoker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJokerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
